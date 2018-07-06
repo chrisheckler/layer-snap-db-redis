d35291ceafdd6e2ba12292542f0aa96babebd2a5
Chris Heckler <hecklerchris@hotmail.com>
2018-07-06 00:14:38 -0700

    snap-db-redis template and helper functions

--- reactive/snap-db-redis.py
+++ reactive/snap-db-redis.py
@@ -2,6 +2,7 @@ from charms.reactive import (
     when,
     when_not,
     set_flag,
+    clear_flag,
     endpoint_from_flag,
 )
 
@@ -12,31 +13,36 @@ from charmhelpers.core import unitdata
 
 REDIS_OUT = '/home/ubuntu/redis_config.txt'
 PGSQL_OUT = '/home/ubuntu/postgreSQL_config.txt'
+FLASK_OUT = '/home/ubuntu/flask_config.txt'
+
 KV = unitdata.kv()
 
 
-@when_not('snap.flask-gunicorn-nginx.installed')
-def snap_installed():
-    """ Snap installed 
+@when_not('snap-db-redis.conf.dir.available')
+def create_data_api_conf_dir():
+    """Ensure config dir exists
     """
+    if not os.path.isdir(SU_CONF_DIR):
+        os.makedirs(SU_CONF_DIR, mode=0o644, exist_ok=True)
+    chownr(SU_CONF_DIR, owner='www-data', group='www-data')
+    set_flag('pdl-api.conf.dir.available')
 
-    status_set('active', 'Snap flask-gunicorn-nginx installed')
-    log('Snap Installed')
-    set_flag('snap.flask-gunicorn-nginx.installed')
-    
 
+
+# ############ PostgreSQL Relations ##############
 @when('pgsql.connected')
 @when_not('snap-db-redis.pgsql.requested')
 def request_database():
-    """ Requesting snap-db-redis pgsql
+    """When connection established to postgres,
+    request a database.
     """
 
     status_set('maintenance', 'Requesting PostgreSQL database')
     pgsql = endpoint_from_flag('pgsql.connected')
     pgsql.set_database('snap-db-redis')
-    
+
     log('Database Available')
-    status_set('active','pgsql.requested')
+    status_set('active', 'pgsql.requested')
     set_flag('snap-db-redis.pgsql.requested')
 
 
@@ -44,81 +50,67 @@ def request_database():
       'snap-db-redis.pgsql.requested')
 @when_not('snap-db-redis.pgsql.available')
 def save_database_connection_info():
-    """ Saves the config data to unitdata
-    """ 
+    """Save the database connection info to the charm unitdata.
+    """
 
-    status_set('maintenance', 
-               'Getting/Setting snap-db-redis config data')
+    status_set('maintenance',
+               'Getting/Setting snap-db-redis database connection info.')
 
     pgsql = endpoint_from_flag('pgsql.master.available')
-   
+
     KV.set('dbname', pgsql.master.dbname)
     KV.set('dbuser', pgsql.master.user)
     KV.set('dbpass', pgsql.master.password)
     KV.set('dbhost', pgsql.master.host)
     KV.set('dbport', pgsql.master.port)
 
+    log('Snap-db-redis Database Available')
     status_set('active', 'Snap-db-redis database available')
     set_flag('snap-db-redis.pgsql.available')
-    log('Snap-db-redis Database Available')
 
 
 @when('snap-db-redis.pgsql.available')
-@when_not('snap-db-redis.config.available')
+@when_not('snap-db-redis.debugging.config.available')
 def output_database_config():
-    """ Outputting config to snap-db-redis HOME dir for now.
+    """Write out database connection info for debugging
     """
 
-    status_set('maintenance', 'Writing config')
+    status_set('maintenance', 'Writing database connection info')
     db_config = KV.getrange('db')
 
     pgsql_config = \
-        'postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}'.format(**db_config)
+        'postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}'.format(
+            **db_config)
 
     with open(PGSQL_OUT, 'a') as f:
         f.write(pgsql_config)
-    
+
     log('PostgreSQL available')
-    status_set('active', pgsql_config)
-    set_flag('snap-db-redis.config.available')
+    status_set('active','Config file written' )
+    set_flag('snap-db-redis.debugging.config.available')
 
 
-@when('endpoint.redis.connected')
+################ Redis Relations ######################
+@when('endpoint.redis.available')
 @when_not('snap-db-redis.redis.available')
 def get_redis_data():
     """ Get redis data
     """
-    
-    status_set('maintenance', 'Getting snap-db-redis.redis')
-    redis = flag_from_endpoint('endpoint.redis.available')
-    redis.set_database('snap-db-redis.redis')
 
-    KV.set('rdhost', redis.host)
-    KV.set('rdport', redis.port)
-    KV.set('rddb', redis.databases)
+    status_set('maintenance', 'Getting data')
 
-    status_set('active', 'Redis Config Written')
-    log('Redis Config Retrieved')
-    set_flag('snap-db-redis.redis.available') 
-
-@when('snap-db-redis.redis.available')
-@when_not('snap-db-redis.redis.config.available')
-def write_redis_data():    
-    status_set('maintenance', 'Writting redis config')
-    redis_config = KV.getrange('rd')
-    redis_db_config = \
-        'redis://{dbhost}:{dbport}/{rddb}'.format(**redis_config)
+    endpoint = endpoint_from_flag('endpoint.redis.available')
 
     with open(REDIS_OUT, 'a') as f:
-        f.write(redis_db_config)
-
-    status_set('active', redis_config)
-    log('Redis DB Config Available')
-    set_flag('snap-db-redis.redis.config.available') 
-
+        f.write(str(endpoint.relation_data()))
+    set_flag('snap-db-redis.redis.available')
+    status_set('active', 'Redis Config Received')
+    log(str(endpoint.relation_data()))
 
 
-
+@when('endpoint.redis.broken')
+def clear_redis_available_flag():
+    clear_flag('snap-db-redis.redis.available')
 
 
 
